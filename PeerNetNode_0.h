6403ab74d7a1303291ac062c69e30ce56817d9d2
HP <HP@DESKTOP-USALVA9>
2020-02-10 15:57:57 +0300

    Добавьте файлы проекта.

---
+++ PeerNetNode_0.h
@@ -0,0 +1,32 @@
+﻿
+// PeerNetNode.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CPeerNetNodeApp:
+// Сведения о реализации этого класса: PeerNetNode.cpp
+//
+
+class CPeerNetNodeApp : public CWinApp
+{
+public:
+	CPeerNetNodeApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPeerNetNodeApp theApp;
