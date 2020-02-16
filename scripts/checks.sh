912a683d9ddf6e64483624a78cdbe74aec3503cb
D-I-R <47750046+D-I-R@users.noreply.github.com>
2020-02-16 21:59:23 +0300

    Delete checks.sh

--- scripts/checks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# shellcheck disable=SC2006
-files=`find . -name "*.cpp" -or -name ".h"`
-filter=-build/c++11,-runtime/references,-whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order
-echo "$files" | xargs cpplint --filter=$filter
-export CTEST_OUTPUT_ON_FAILURE=true
